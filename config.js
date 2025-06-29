c474d3b6cc4a7cb49ec1a21d13fc1f06bbe35dbc
cejas122 <llamasappservices@gmail.com>
2025-06-28 19:21:58 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://ldqimmfgnqcgrpkawgsp.supabase.co';
-const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkcWltbWZnbnFjZ3Jwa2F3Z3NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwNjY2MTcsImV4cCI6MjA2NjY0MjYxN30.FeI1vftIhiCzUcNUF9S0wbTxA0Z061HBvphMFmK1xCM';
+export const supabaseUrl = 'https://ldqimmfgnqcgrpkawgsp.supabase.co';
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkcWltbWZnbnFjZ3Jwa2F3Z3NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwNjY2MTcsImV4cCI6MjA2NjY0MjYxN30.FeI1vftIhiCzUcNUF9S0wbTxA0Z061HBvphMFmK1xCM';
 
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
